a73bd194eec8d2d623b981bb3f11001a7009d8f4
HP <HP@LAPTOP-UK475GCQ>
2025-03-19 12:34:22 +0530

    FIRST COMMIT

---
+++ Samplemavenproject/src/main/java/pack/InstanceMethod.java
@@ -0,0 +1,25 @@
+package pack;
+
+import pack.InstanceMethod;
+
+public class InstanceMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InstanceMethod show=new  InstanceMethod();        
+		  show.print(30,20);
+		  show.print("hello");  
+		
+	}
+	public  void print(int a,int b)
+	{                                       
+	int c=a+b;
+	System.out.println(c);
+	}
+	public  void print(String a)
+	{
+	System.out.println(a);
+	}
+	}
+
+
